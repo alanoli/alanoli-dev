24ff91cd978d96bdbaebbdf4009b23c53381133a
Alan Oliveira <alancesar2007@gmail.com>
2020-06-07 11:40:30 -0300

    Updates

---
+++ precache-manifest.9cc7c6bae6aff147b262da5305832a52.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb990af7a2df0de335b6c227f30e94b1",
+    "url": "/alanoli-dev/index.html"
+  },
+  {
+    "revision": "84d81432f0ca1d28c95b",
+    "url": "/alanoli-dev/static/css/2.7bc72fb5.chunk.css"
+  },
+  {
+    "revision": "602bd1f1278787e0879a",
+    "url": "/alanoli-dev/static/css/main.eb3646db.chunk.css"
+  },
+  {
+    "revision": "84d81432f0ca1d28c95b",
+    "url": "/alanoli-dev/static/js/2.ea226c84.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/alanoli-dev/static/js/2.ea226c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602bd1f1278787e0879a",
+    "url": "/alanoli-dev/static/js/main.f17bd194.chunk.js"
+  },
+  {
+    "revision": "e65cc8df7f82448b4808",
+    "url": "/alanoli-dev/static/js/runtime-main.78104ba5.js"
+  },
+  {
+    "revision": "9e977f3194c410d0c8d7fc6aba859e0c",
+    "url": "/alanoli-dev/static/media/background.9e977f31.jpg"
+  },
+  {
+    "revision": "bd34687255a10e4b0d4707537d160fbd",
+    "url": "/alanoli-dev/static/media/background_2.bd346872.jpg"
+  },
+  {
+    "revision": "b30a2d2e7c3caac3c3f9ba3bd96483b4",
+    "url": "/alanoli-dev/static/media/css3.b30a2d2e.png"
+  },
+  {
+    "revision": "d2a9e9a37d9d5cd134816a0623953114",
+    "url": "/alanoli-dev/static/media/docker.d2a9e9a3.png"
+  },
+  {
+    "revision": "44a2a8283145b3b154d391fe12caa1f4",
+    "url": "/alanoli-dev/static/media/foto.44a2a828.png"
+  },
+  {
+    "revision": "cace8f8339506e4137693a5d1c33a8f3",
+    "url": "/alanoli-dev/static/media/html.cace8f83.png"
+  },
+  {
+    "revision": "bdd029719ccf22d1ec6e0afc4e488d76",
+    "url": "/alanoli-dev/static/media/javascript.bdd02971.png"
+  },
+  {
+    "revision": "61a30cd48504de916dc2bac418906a34",
+    "url": "/alanoli-dev/static/media/react.61a30cd4.png"
+  },
+  {
+    "revision": "2afcb71a8360d7da78618911811fcc5d",
+    "url": "/alanoli-dev/static/media/sql.2afcb71a.png"
+  }
+]);
